8deb475d4ef2be1624a1324f5332b2d50bc8f506
Сергей Пчелинцев <s.pchelintcev@yandex.ru>
2022-08-25 19:59:45 +0300

    добавление maxNumberWords

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,11 @@ public class Main {
 
         List<Person> artPeople = new ArrayList<>();
 
-        artPeople.add(new Person("Rembrandt", "Harmenszoon van Rijn", 63));
+        artPeople.add(new Person("Rembrandt", "Harmenszoon Rijn", 63));
         artPeople.add(new Person("Leonardo", "di ser Piero da Vinci", 67));
         artPeople.add(new Person("Michelangelo", "di Buonarroti Simoni", 88));
         artPeople.add(new Person("Rogier", "van der Weyden", 64));
-        artPeople.add(new Person("Alessandro", "di Vanni Filipepi", 65));
+        artPeople.add(new Person("Alessandro", "di Filipepi", 65));
         artPeople.add(new Person("Raffael", "da Urbino Santi", 37));
 
         System.out.println("Несортированный список:");
@@ -21,7 +21,7 @@ public class Main {
         }
         System.out.println();
         System.out.println("Сортировка компоратором:");
-        Collections.sort(artPeople, new PersonComparator());
+        Collections.sort(artPeople, new PersonComparator(3));
         for (Person artPerson : artPeople) {
             System.out.println(artPerson);
         }
--- src/PersonComparator.java
+++ src/PersonComparator.java
@@ -1,12 +1,30 @@
 import java.util.Comparator;
 
 public class PersonComparator implements Comparator<Person> {
+
+    public static int maxNumberWords;
+
+    public PersonComparator(int maxNumberWords) {
+        this.maxNumberWords = maxNumberWords;
+    }
+
     @Override
+    //TODO без использования maxNumberWords:
+//    public int compare(Person o1, Person o2) {
+//        if (o1.numberWords() < o2.numberWords()) {
+//            return 1;
+//        } else if (o1.numberWords() > o2.numberWords()) {
+//            return -1;
+//        } else return Integer.compare(o2.getAge(), o1.getAge());
+//    }
+    //TODO с использованием maxNumberWords:
     public int compare(Person o1, Person o2) {
-        if (o1.numberWords() < o2.numberWords()) {
-            return 1;
-        } else if (o1.numberWords() > o2.numberWords()) {
-            return -1;
-        } else return Integer.compare(o1.getAge(), o2.getAge());
+        if(o1.numberWords() <= maxNumberWords && o2.numberWords() <= maxNumberWords){
+            if (o1.numberWords() < o2.numberWords()) {
+                return 1;
+            } else if (o1.numberWords() > o2.numberWords()) {
+                return -1;
+            } else return Integer.compare(o2.getAge(), o1.getAge());
+        }else return Integer.compare(o2.getAge(), o1.getAge());
     }
 }
