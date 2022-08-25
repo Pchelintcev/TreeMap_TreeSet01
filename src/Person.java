3c5276d7c57153caf8b5bec72414e243f0b5203c
Сергей Пчелинцев <s.pchelintcev@yandex.ru>
2022-08-25 18:01:13 +0300

    + компоратор по возрасту

--- src/Person.java
+++ src/Person.java
@@ -5,39 +5,44 @@ public class Person {
     private int age;
 
     //TODO создайте конструктор на все поля
-    public Person(String name, String surname, int age){
+    public Person(String name, String surname, int age) {
         this.name = name;
         this.surname = surname;
-        this.age = age;  
+        this.age = age;
     }
 
     //TODO создайте геттеры и сеттеры
-    public String getName(){
+    public String getName() {
         return name;
     }
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
-    public String getSurname(){
+
+    public String getSurname() {
         return name;
     }
-    public void setSurname(String surname){
+
+    public void setSurname(String surname) {
         this.surname = surname;
     }
-    public int getAge(){
+
+    public int getAge() {
         return age;
     }
-    public void setAge(int age){
+
+    public void setAge(int age) {
         this.age = age;
     }
 
     //TODO вычисляем кол-во слов в фамилии:
-    public int numberWords(){
+    public int numberWords() {
         int count = 0;
-        if(surname.length() != 0){
-            count ++;
-            for(int i = 0; i < surname.length(); i++){
-                if(surname.charAt(i) == ' '){
+        if (surname.length() != 0) {
+            count++;
+            for (int i = 0; i < surname.length(); i++) {
+                if (surname.charAt(i) == ' ') {
                     count++;
                 }
             }
@@ -47,7 +52,7 @@ public class Person {
 
     //TODO создайте toString
     @Override
-    public String toString(){
-        return name + " " + surname + " " +  age;
+    public String toString() {
+        return name + " " + surname + " " + age;
     }
 }
--- src/PersonComparator.java
+++ src/PersonComparator.java
@@ -3,14 +3,10 @@ import java.util.Comparator;
 public class PersonComparator implements Comparator<Person> {
     @Override
     public int compare(Person o1, Person o2) {
-        if( o1.numberWords() < o2.numberWords()){
+        if (o1.numberWords() < o2.numberWords()) {
             return 1;
-        }else if(o1.numberWords() > o2.numberWords()){
+        } else if (o1.numberWords() > o2.numberWords()) {
             return -1;
-        }else {
-            Integer.compare(o1.getAge(), o2.getAge());
-        }
-        return 0;
+        } else return Integer.compare(o1.getAge(), o2.getAge());
     }
 }
-    //return Integer.compare(o2.numberWords(), o1.numberWords());
