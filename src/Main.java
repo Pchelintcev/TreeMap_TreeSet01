26e31432ddbdff87c90d0c1a91230b99f2095467
Сергей Пчелинцев <s.pchelintcev@yandex.ru>
2022-08-25 16:49:20 +0300

    компоратор по фамилии

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Person> artPeople = new ArrayList<>();
+
+        artPeople.add(new Person("Rembrandt", "Harmenszoon van Rijn", 63));
+        artPeople.add(new Person("Leonardo", "di ser Piero da Vinci", 67));
+        artPeople.add(new Person("Michelangelo", "di Buonarroti Simoni", 88));
+
+        System.out.println("Несортированный список:");
+        for (Person artPerson : artPeople) {
+            System.out.println(artPerson);
+        }
+        System.out.println();
+        System.out.println("Сортировка компоратором:");
+        Collections.sort(artPeople, new PersonComparator());
+        for (Person artPerson : artPeople) {
+            System.out.println(artPerson);
+        }
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,53 @@
+public class Person {
+
+    private String name;
+    private String surname;
+    private int age;
+
+    //TODO создайте конструктор на все поля
+    public Person(String name, String surname, int age){
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    //TODO создайте геттеры и сеттеры
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getSurname(){
+        return name;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    //TODO вычисляем кол-во слов в фамилии:
+    public int numberWords(){
+        int count = 0;
+        if(surname.length() != 0){
+            count ++;
+            for(int i = 0; i < surname.length(); i++){
+                if(surname.charAt(i) == ' '){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    //TODO создайте toString
+    @Override
+    public String toString(){
+        return name + " " + surname + " " +  age;
+    }
+}
