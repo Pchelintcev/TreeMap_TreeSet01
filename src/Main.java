9a65290fd802b53b8e3b0fb03b47d6338f6ada43
Сергей Пчелинцев <s.pchelintcev@yandex.ru>
2022-09-03 14:39:06 +0300

    реализация предиката

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
 
@@ -14,16 +15,21 @@ public class Main {
         artPeople.add(new Person("Rogier", "van der Weyden", 64));
         artPeople.add(new Person("Alessandro", "di Filipepi", 65));
         artPeople.add(new Person("Raffael", "da Urbino Santi", 37));
+        artPeople.add(new Person("Abremo", "Marco van Basten fon Svishen", 16));
+        artPeople.add(new Person("Ronald", "Craman van der Sar", 17));
+        artPeople.add(new Person("Elena", "Craman van der Sar", 18));
 
         System.out.println("Несортированный список:");
-        for (Person artPerson : artPeople) {
-            System.out.println(artPerson);
+        for (Person P : artPeople) {
+            System.out.println(P);
         }
         System.out.println();
-        System.out.println("Сортировка компоратором:");
-        Collections.sort(artPeople, new PersonComparator(3));
-        for (Person artPerson : artPeople) {
-            System.out.println(artPerson);
+
+        System.out.println("Сортировка предикатом:");
+        Predicate<Person> predicate = Person -> Person.getAge() < 18;
+        artPeople.removeIf(predicate);
+        for (Person P : artPeople) {
+            System.out.println(P);
         }
     }
 }
